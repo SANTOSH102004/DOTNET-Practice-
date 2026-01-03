b4244e56e35123cd86d1abc08361633ee973a0f9
Santosh Kumbhar <80945@MANINDSHO.ORG>
2026-01-03 16:22:10 +0530

    dotnet practice

---
+++ DP/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class DP
+{
+    static bool IsPalindrome(string s, int start, int end)
+    {
+        while (start < end)
+        {
+            if (s[start] != s[end])
+                return false;
+            start++;
+            end--;
+        }
+        return true;
+    }
+
+    static int MinCut(string s)
+    {
+        int n = s.Length;
+        int[] dp = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            if (IsPalindrome(s, 0, i))
+            {
+                dp[i] = 0;
+            }
+            else
+            {
+                dp[i] = int.MaxValue;
+                for (int j = 0; j < i; j++)
+                {
+                    if(IsPalindrome(s, j + 1, i))
+                    {
+                        dp[i] = Math.Min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+        return dp[n - 1];
+    }
+
+    static void Main()
+    {
+        string s = "aab";
+        Console.WriteLine( "Minimum cuts needed:" + MinCut(s));
+    }
+}
---
+++ First/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class BurstBalloons
+{
+    public static int MaxCoins(int[] nums)
+    {
+        int n = nums.Length;
+        int[] arr = new int[n + 2];
+        arr[0] = arr[n + 1] = 1;
+
+        for (int i = 0; i < n; i++)
+            arr[i + 1] = nums[i];
+
+        int[,] dp = new int[n + 2, n + 2];
+
+        for (int length = 1; length <= n; length++)
+        {
+            for (int left = 1; left <= n - length + 1; left++)
+            {
+                int right = left + length - 1;
+
+                for (int k = left; k <= right; k++)
+                {
+                    int coins = arr[left - 1] * arr[k] * arr[right + 1];
+                    coins += dp[left, k - 1] + dp[k + 1, right];
+                    dp[left, right] = Math.Max(dp[left, right], coins);
+                }
+            }
+        }
+
+        return dp[1, n];
+    }
+
+    static void Main()
+    {
+        int[] nums = { 3, 1, 5, 8 };
+        Console.WriteLine("Maximum coins: " + MaxCoins(nums));
+    }
+}
